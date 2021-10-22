68b0bf1b1ef37f93bb6bf55f54c724f3cfdc424e
thomasokubo <tai.okubo93@gmail.com>
2021-10-22 18:25:23 -0300

    Initial commit

---
+++ src/main/java/com/tto/yaus/YausApplication.java
@@ -0,0 +1,13 @@
+package com.tto.yaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YausApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tto/yaus/YausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tto.yaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
